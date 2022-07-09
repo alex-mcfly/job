183907b0a8824f6d2cdba30a046e6994ab78f160
Alex <bel.am.2004@gmail.com>
2022-07-09 12:41:53 +0300

    delete old files

--- task1/index.py
+++
@@ -1,8 +0,0 @@
-# print(task("111111111110000000000000000"))
-# >> OUT: 11
-
-def task(array):
-    return array.index('0')
-
-if __name__ == '__main__':
-    print(task('111111111110000000000000000'))
